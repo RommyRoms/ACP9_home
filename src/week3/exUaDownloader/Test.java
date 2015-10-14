ecb336ee9ca627ce5eeffcbf4f65898fda6cd702
Roma <roma.gaming@gmail.com>
2015-10-14 16:22:33 +0300

    add w3 ex.ua downloader

---
+++ src/week3/exUaDownloader/Test.java
@@ -0,0 +1,11 @@
+package week3.exUaDownloader;
+
+
+import week3.exUaDownloader.view.View;
+
+public class Test {
+    public static void main(String[] args) {
+        Controller controller = new Controller(new View());
+    }
+
+}
---
+++ src/week3/exUaParser/ParserByJsoup.java
@@ -0,0 +1,43 @@
+package week3.exUaParser;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+
+
+public class ParserByJsoup {
+    public static final String DIRECTORY = "D:/def/";
+    public static String url = "http://www.ex.ua/94704559?r=3,23776";
+    public static Map<String,String> map = new TreeMap<>();
+
+    public static void main(String[] args) {
+
+        try {
+            Document document = Jsoup.connect(url).get();
+            Elements links = document.select("a[href*=/get]");
+
+            for (Element link : links) {
+                String href = link.attr("href");
+                String title = link.text();
+                if (title.contains(".mp3")) {
+                    map.put(href,title);
+                }
+            }
+
+            for (Map.Entry<String ,String > entry: map.entrySet()){
+                byte[] arr = IOUtils.getByteArr(new URL("http://www.ex.ua"+entry.getKey()).openStream());
+                IOUtils.writeBytes(arr,DIRECTORY+entry.getValue());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
